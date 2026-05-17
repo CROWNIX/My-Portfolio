dd43f62bc8bc81eefe0064195f5432f2e4ed3bb5
CROWNIX <rahmatfauzi841@gmail.com>
2026-05-17 22:08:10 +0700

    fix failed build

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,95 +1,95 @@
-import React from 'react';
+import React from "react";
 import {
-    Nav,
-    NavLink,
-    NavbarContainer,
-    Span,
-    NavLogo,
-    NavItems,
-    GitHubButton,
-    ButtonContainer,
-    MobileIcon,
-    MobileMenu,
-    MobileLink,
-} from './NavbarStyledComponent';
-import { DiCssdeck } from 'react-icons/di';
-import { FaBars } from 'react-icons/fa';
-import { Bio } from '../../data/constants';
-import { useTheme } from 'styled-components';
+  Nav,
+  NavLink,
+  NavbarContainer,
+  Span,
+  NavLogo,
+  NavItems,
+  GitHubButton,
+  ButtonContainer,
+  MobileIcon,
+  MobileMenu,
+  MobileLink,
+} from "./NavbarStyledComponent";
+import { DiCssdeck } from "react-icons/di";
+import { FaBars } from "react-icons/fa";
+import { Bio } from "../../data/constants";
+import { useTheme } from "styled-components";
 
 const Navbar = () => {
-    const [isOpen, setIsOpen] = React.useState(false);
-    const theme = useTheme();
-    return (
-        <Nav>
-            <NavbarContainer>
-                <NavLogo to="/">
-                    <a
-                        style={{
-                            display: 'flex',
-                            alignItems: 'center',
-                            color: 'white',
-                            marginBottom: '20;',
-                            cursor: 'pointer',
-                        }}
-                    >
-                        <DiCssdeck size="3rem" /> <Span>Rahmat Fauzi</Span>
-                    </a>
-                </NavLogo>
-                <MobileIcon>
-                    <FaBars
-                        onClick={() => {
-                            setIsOpen(!isOpen);
-                        }}
-                    />
-                </MobileIcon>
-                <NavItems>
-                    <NavLink href="#about">Tentang</NavLink>
-                    <NavLink href="#skills">Skills</NavLink>
-                    <NavLink href="#experience">Pengalaman</NavLink>
-                    <NavLink href="#projects">Projects</NavLink>
-                    {/* <NavLink href="#education">Edukasi</NavLink> */}
-                </NavItems>
-                <ButtonContainer>
-                    <GitHubButton href={Bio.github} target="_blank">
-                        Github Profile
-                    </GitHubButton>
-                </ButtonContainer>
-                {isOpen && (
-                    <MobileMenu isOpen={isOpen}>
-                        <MobileLink
-                            href="#about"
-                            onClick={() => {
-                                setIsOpen(!isOpen);
-                            }}
-                        >
-                            Tentang
-                        </MobileLink>
-                        <MobileLink
-                            href="#skills"
-                            onClick={() => {
-                                setIsOpen(!isOpen);
-                            }}
-                        >
-                            Skills
-                        </MobileLink>
-                        <MobileLink
-                            href="#experience"
-                            onClick={() => {
-                                setIsOpen(!isOpen);
-                            }}
-                        >
-                            Pengalaman
-                        </MobileLink>
-                        <MobileLink
-                            href="#projects"
-                            onClick={() => {
-                                setIsOpen(!isOpen);
-                            }}
-                        >
-                            Projects
-                        </MobileLink>
-                        {/* <MobileLink
+  const [isOpen, setIsOpen] = React.useState(false);
+  const theme = useTheme();
+  return (
+    <Nav>
+      <NavbarContainer>
+        <NavLogo to="/">
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              color: "white",
+              marginBottom: "20px",
+              cursor: "pointer",
+            }}
+          >
+            <DiCssdeck size="3rem" /> <Span>Rahmat Fauzi</Span>
+          </div>
+        </NavLogo>
+        <MobileIcon>
+          <FaBars
+            onClick={() => {
+              setIsOpen(!isOpen);
+            }}
+          />
+        </MobileIcon>
+        <NavItems>
+          <NavLink href="#about">Tentang</NavLink>
+          <NavLink href="#skills">Skills</NavLink>
+          <NavLink href="#experience">Pengalaman</NavLink>
+          <NavLink href="#projects">Projects</NavLink>
+          {/* <NavLink href="#education">Edukasi</NavLink> */}
+        </NavItems>
+        <ButtonContainer>
+          <GitHubButton href={Bio.github} target="_blank">
+            Github Profile
+          </GitHubButton>
+        </ButtonContainer>
+        {isOpen && (
+          <MobileMenu isOpen={isOpen}>
+            <MobileLink
+              href="#about"
+              onClick={() => {
+                setIsOpen(!isOpen);
+              }}
+            >
+              Tentang
+            </MobileLink>
+            <MobileLink
+              href="#skills"
+              onClick={() => {
+                setIsOpen(!isOpen);
+              }}
+            >
+              Skills
+            </MobileLink>
+            <MobileLink
+              href="#experience"
+              onClick={() => {
+                setIsOpen(!isOpen);
+              }}
+            >
+              Pengalaman
+            </MobileLink>
+            <MobileLink
+              href="#projects"
+              onClick={() => {
+                setIsOpen(!isOpen);
+              }}
+            >
+              Projects
+            </MobileLink>
+            {/* <MobileLink
                             href="#education"
                             onClick={() => {
                                 setIsOpen(!isOpen);
@@ -97,23 +97,23 @@ const Navbar = () => {
                         >
                             Edukasi
                         </MobileLink> */}
-                        <GitHubButton
-                            style={{
-                                padding: '10px 16px',
-                                background: `${theme.primary}`,
-                                color: 'white',
-                                width: 'max-content',
-                            }}
-                            href={Bio.github}
-                            target="_blank"
-                        >
-                            Profile Github
-                        </GitHubButton>
-                    </MobileMenu>
-                )}
-            </NavbarContainer>
-        </Nav>
-    );
+            <GitHubButton
+              style={{
+                padding: "10px 16px",
+                background: `${theme.primary}`,
+                color: "white",
+                width: "max-content",
+              }}
+              href={Bio.github}
+              target="_blank"
+            >
+              Profile Github
+            </GitHubButton>
+          </MobileMenu>
+        )}
+      </NavbarContainer>
+    </Nav>
+  );
 };
 
 export default Navbar;
